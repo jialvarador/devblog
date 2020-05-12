acc27d5eb5b09352fa52a3b2fa7bec15857327d5
Jose Inocencio Alvarado <jose.alvarado@infotec.mx>
2020-05-12 09:30:12 -0500

    actualizaciones

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Jose Inocencio Alvarado Rodriguez', // Required
+  description: 'Java Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    //website: 'https://ryanfitzgerald.ca',
+    github: 'https://github.com/jialvarador',
+    twitter: 'https://twitter.com/josealvarado22',
+    //linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
